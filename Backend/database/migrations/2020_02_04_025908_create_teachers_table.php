f77d77f722bf6788f9f7a0f0b342af86660bfe24
João Pedro Silva <joaopedro@MacBook-Air-de-Joao.local>
2020-02-04 00:10:39 -0300

    +Inicio Back

---
+++ Backend/database/migrations/2020_02_04_025908_create_teachers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('number');
+            $table->integer('birth');
+            $table->integer('CPF');
+            $table->float('lesson_price');
+            $table->float('rent_price')->nullable();
+            $table->longText('description');
+            $table->string('district');
+            $table->string('zone');
+            $table->string('instruments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
