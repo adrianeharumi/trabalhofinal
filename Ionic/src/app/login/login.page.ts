d76ff47164e17726cb4d0652263bd5ad281e928a
Julia Lopes <jujulrocha@gmail.com>
2020-02-10 23:18:16 -0300

    Rota submit->home

--- Ionic/src/app/login/login.page.ts
+++ Ionic/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginPage implements OnInit {
 
   registerForm: FormGroup;
 
-  constructor(public formbuilder: FormBuilder) {
+  constructor(public formbuilder: FormBuilder,  public router: Router) {
 
     this.registerForm = this.formbuilder.group({
 
@@ -27,6 +28,7 @@ export class LoginPage implements OnInit {
   submit(form){
     console.log(form);
     console.log(form.value);
+    this.router.navigate(['/tabs/tab1']);
 
   }
 }
